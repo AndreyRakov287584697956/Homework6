f81a6f54371515775e4c034d17644137bb726abb
AndreyRakov287584697956 <andreyrakov266@gmail.com>
2025-07-21 12:09:02 +0300

    Задание 6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
-
 public class Main {
     public static void main(String[] args) {
-
+        int[]array = {6, 96, 78, 43, 55, 12};
+        System.out.println("Исходный массив:");
+        for (int i: array){ //Вывод через for-each
+            System.out.println(i);
+        }
+        System.out.println();
+        System.out.println("Изменённый массив:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= 2;
+            System.out.println(array[i]);
+        }
     }
 }
